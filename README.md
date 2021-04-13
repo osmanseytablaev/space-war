7e4ec75c8a581f0a39d6450c54c437a1c5cff471
osmanseytablaev <osman.seytablaev@gmail.com>
2021-04-13 16:04:17 +0300

    Update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # SPACE-WAR
+
+## Install
+NOTES:
+OS UBUNTU LINUX
+
+```bash
+    Install Python
+    sudo apt install python3-pip
+    
+    Install Pygame
+    sudo apt-get install python-pygame
+    
+```
+
+## Run game
+```bash
+    python3 space_war.py
+```
+
+
+## Rules game
+```bash
+    Управление: стрелками;
+    Стрельба: пробел.
+    
+    У тебя есть 3 жизни.
+    Если ты набрал 30 очков ты победил.
+```
--- space_war.py
+++ space_war.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import datetime as dt
+import time
 
 
 class Ship:
@@ -21,7 +22,7 @@ class Ship:
         pygame.draw.rect(self.game.screen,
                          (254, 52, 110),
                          pygame.Rect(self.gun_x, self.gun_y, 2, 4))
-        self.gun_y -= 5
+        self.gun_y -= 1
 
 
 class Enemy:
@@ -34,7 +35,7 @@ class Enemy:
     def show(self):
         enemy = pygame.image.load('Images/enemy.png')
         self.game.screen.blit(enemy, (self.enemy_x, self.enemy_y))
-        self.enemy_y += 0.20
+        self.enemy_y += 0.35
 
     def el_destroyer0_0(self, game):
         for bullet in game.bullets:
@@ -42,9 +43,15 @@ class Enemy:
                     self.enemy_y + self.size > bullet.gun_y > self.enemy_y - self.size):
                 game.bullets.remove(bullet)
                 game.enemies.remove(self)
-                pygame.mixer.music.load('Audio/boom2.mp3')
+                pygame.mixer.music.load('Audio/boom.ogg')
                 pygame.mixer.music.play()
                 return True
+
+    def des(self, game):
+        if self.enemy_y >= 700:
+            game.enemies.remove(self)
+            return True
+
 
 
 class Game:
@@ -53,52 +60,74 @@ class Game:
 
     def check_exit(self):
         date_game = dt.datetime.now() - self.time_game
-        if date_game.seconds >= 300:  # 60 * 5 = 300 sec = 5 min game
+        if date_game.seconds >= 180:  # 60 * 5 = 300 sec = 5 min game
             return True
         else:
             return False
 
     def __init__(self, w, h):  # Constructor
         pygame.init()
-        pygame.mixer.music.load('Audio/A4.mp3')
+        pygame.mixer.init()
+        pygame.mixer.music.load('Audio/start.ogg')
         pygame.mixer.music.play()
         self.w = w
         self.h = h
-        pygame.display.set_caption("SPACE WAR")
+        pygame.display.set_caption("Space War")
         self.screen = pygame.display.set_mode((w, h))
         ship = Ship(self, 450, 650)
         enemy = Enemy(self, random.randint(1, 500), 0.99)
         self.time = dt.datetime.now()
         self.time_game = dt.datetime.now()
         self.score_value = 0
+        self.life = 3
         self.YELLOW = 255, 255, 0
         while True:
             if self.check_exit():
                 print('Game end')
                 break
             date = dt.datetime.now() - self.time
-            if date.seconds >= 5:
+            if date.seconds >= 1.3:
                 self.enemies.append(Enemy(self, random.randint(1, 800), enemy.enemy_y))
                 self.time = dt.datetime.now()
             f1 = pygame.font.SysFont('serif', 16)
             text1 = f1.render("Score:" + str(self.score_value), True,
                               self.YELLOW)
             self.screen.blit(text1, (820, 670))
+            f2 = pygame.font.SysFont('serif', 16)
+            text2 = f2.render("Life:" + str(self.life), True,
+                              self.YELLOW)
+            self.screen.blit(text2, (820, 650))
             ship.show()
             pygame.display.flip()
             self.screen.fill((0, 0, 0))
             pressed = pygame.key.get_pressed()
             if pressed[pygame.K_LEFT]:
-                ship.ship_x -= 2 if ship.ship_x > 20 else 0
+                ship.ship_x -= 0.50 if ship.ship_x > 20 else 0
             elif pressed[pygame.K_RIGHT]:
-                ship.ship_x += 2 if ship.ship_x < self.w - 20 else 0
+                ship.ship_x += 0.50 if ship.ship_x < self.w - 20 else 0
             for i in pygame.event.get():
                 if i.type == pygame.QUIT:
                     exit()
                 if i.type == pygame.KEYDOWN and i.key == pygame.K_SPACE:
                     self.bullets.append(Ship(self, ship.gun_x, ship.gun_y))
-                    pygame.mixer.music.load("Audio/boom.mp3")
+                    pygame.mixer.music.load("Audio/laser.ogg")
                     pygame.mixer.music.play()
+            if self.life == 0:
+                f3 = pygame.font.SysFont('serif', 50)
+                text3 = f3.render("GAME OVER", True,
+                                  (255, 0, 0))
+                self.screen.blit(text3, (350, 300))
+                if date.seconds >= 2:
+                    time.sleep(3)
+                    exit()
+            if self.score_value == 30:
+                f4 = pygame.font.SysFont('serif', 50)
+                text4 = f4.render("YOU WON!", True,
+                                  (255, 0, 0))
+                self.screen.blit(text4, (350, 300))
+                if date.seconds >= 2:
+                    time.sleep(3)
+                    exit()
             for bullet in self.bullets:
                 bullet.fire()
             for enemy_state in self.enemies:
@@ -106,6 +135,10 @@ class Game:
                 if enemy_state.el_destroyer0_0(self):
                     self.score_value += 1
                     self.screen.blit(text1, (820, 670))
+            for enemy_st in self.enemies:
+                if enemy_st.des(self):
+                    self.life -= 1
+                    self.screen.blit(text2, (820, 650))
 
 
 if __name__ == '__main__':
